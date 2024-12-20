a5d72196ea3793aa509ead45acb8656fcf56e096
Sushma Sharma <154212342+SumiSushmA@users.noreply.github.com>
2024-12-20 13:58:11 +0545

    update home_screen

--- lib/home screen/home_screen.dart
+++ lib/home screen/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:glownepal_mobile_app_5th_sem/home%20screen/about_us_screen_view.dart';
-
-import 'profile_screen.dart';
+import 'package:glownepal_mobile_app_5th_sem/home%20screen/home_page_view.dart';
+import 'package:glownepal_mobile_app_5th_sem/home%20screen/profile_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -14,7 +14,7 @@ class _HomeScreenState extends State<HomeScreen> {
   int _currentIndex = 0;
 
   final List<Widget> _screens = [
-    const Center(child: Text('Home Screen', style: TextStyle(fontSize: 24))),
+    const HomePageView(),
     const Center(child: Text('Stylish Screen', style: TextStyle(fontSize: 24))),
     const AboutUsScreenView(),
     const ProfileScreen(),
